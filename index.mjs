823e61fe3a1294e76ab83bcc1336ca79d0fcfc00
cieszynski <admin@cieszynski.de>
2022-06-21 21:06:51 +0200

    initial commit

---
+++ index.mjs
@@ -0,0 +1,8 @@
+import * as fs from 'fs/promises';
+
+try {
+  await fs.unlink('/tmp/hello');
+  console.log('successfully deleted /tmp/hello');
+} catch (error) {
+  console.error('there was an error:', error.message);
+}
