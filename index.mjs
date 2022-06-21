37d507092520c30b873e5ef62491332c7eec9a7b
cieszynski <admin@cieszynski.de>
2022-06-21 21:53:20 +0200

    run build 3

--- index.mjs
+++ index.mjs
@@ -3,8 +3,15 @@
 import * as fs from 'fs/promises';
 
 try {
+  const content = 'Some content!';
+  await fs.writeFile('index.html', "Hello World!");
+} catch (err) {
+  console.log(err);
+}
+
+/* try {
   await fs.unlink('/tmp/hello');
   console.log('successfully deleted /tmp/hello');
 } catch (error) {
   console.error('there was an error:', error.message);
-}
+} */
